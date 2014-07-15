44736a7b4fc7650f7cb45316e666890632c6d78c
Christopher Miller <cmiller730@gmail.com>
2014-07-14 21:51:50 -0400

    Remove small hint from functions.
    Better for the user to decide when its not worth parallelizing
    something. Inline leaf functions. Add bang patterns.

--- Numeric/Core/Array.hs
+++ Numeric/Core/Array.hs
@@ -11,7 +11,6 @@ module Numeric.Core.Array (
   ) where
 
 import Data.Array.Repa as R
-import Data.Array.Repa.Repr.HintSmall
 import Data.Typeable.Internal
 import Prelude as P
 import Control.Exception
@@ -24,41 +23,44 @@ instance Show NumHSArrayException where
   show ArangeDivisionByZero = "Arange: Step size must be non-zero"
   show EyeNegativeDimension = "Eye: negative dimensions are not allowed."
 
-linspace :: Double -> Double -> Int -> Array (S D) DIM1 Double
+linspace :: Double -> Double -> Int -> Array D DIM1 Double
 linspace !start !stop !num
-  | num < 0 = hintSmall $ R.fromFunction (Z:.0) (\_ -> 0)
-  | num == 1 = hintSmall $ R.fromFunction (Z:.1) (\_ -> start)
+  | num < 0 = R.fromFunction (Z:.0) (\_ -> 0)
+  | num == 1 = R.fromFunction (Z:.1) (\_ -> start)
   | otherwise = let delta = (stop - start)/(fromIntegral num - 1)
                     fun :: DIM1 -> Double
                     {-# INLINE fun #-}
-                    fun (Z:.idx) = start + (fromIntegral idx)*delta
-                in hintSmall $ R.fromFunction (Z:.num) fun               
+                    fun !(Z:.idx) = start + (fromIntegral idx)*delta
+                in R.fromFunction (Z:.num) fun               
                   
-logspace :: Double -> Double -> Double -> Int -> Array (S D) DIM1 Double
+logspace :: Double -> Double -> Double -> Int -> Array D DIM1 Double
 logspace !start !stop !base !num = 
   let x0 = linspace start stop num
-  in hintSmall $ R.map (base**) x0 
+  in R.map (base**) x0 
      
-arange :: Double -> Double -> Double -> Array (S D) DIM1 Double
+arange :: Double -> Double -> Double -> Array D DIM1 Double
 arange !start !stop !delta 
   | delta == 0 = throw ArangeDivisionByZero
   | otherwise = linspace start (start+(fromIntegral num - 1)*delta) num
   where
     num = ceiling $ (stop - start)/delta
 
-eye :: Int -> Array (S D) DIM2 Double
+eye :: Int -> Array D DIM2 Double
 eye !n | n < 0 = throw EyeNegativeDimension
-       | otherwise = hintSmall $ R.fromFunction (Z:.n:.n) delta
+       | otherwise = R.fromFunction (Z:.n:.n) delta
         where 
           delta :: DIM2 -> Double
           {-# INLINE delta #-}
-          delta (Z:.i:.j) = if (i==j) then 1 else 0
+          delta !(Z:.i:.j) = if (i==j) then 1 else 0
 
-scalarInit :: (Shape sh) => sh -> Double -> Array (S D) sh Double
-scalarInit !sh !x = hintSmall $ R.fromFunction sh (\_ -> x)
+scalarInit :: (Shape sh) => sh -> Double -> Array D sh Double
+scalarInit !sh !x = R.fromFunction sh f
+                    where
+                      {-# INLINE f #-}
+                      f _ = x
 
-zeros :: (Shape sh) => sh -> Array (S D) sh Double 
+zeros :: (Shape sh) => sh -> Array D sh Double 
 zeros !sh = scalarInit sh 0.0
 
-ones :: (Shape sh) => sh -> Array (S D) sh Double
+ones :: (Shape sh) => sh -> Array D sh Double
 ones !sh = scalarInit sh 1.0
